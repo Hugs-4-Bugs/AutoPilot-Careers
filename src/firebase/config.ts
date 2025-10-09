5d2b215e4b8b3f0ac1abb2daf4e6e3cfb6cd7933
Prabhat Kumar <prabhatkumarssm72@gmail.com>
2025-10-09 12:38:31 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,8 @@
-// DO NOT EDIT, this file is generated
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-
-const firebaseConfig = {"projectId":"autopilot-careers-de69a","appId":"1:1012708335965:web:15848c697067f92080a256","storageBucket":"autopilot-careers-de69a.appspot.com","apiKey":"mock-key-for-dev-env","authDomain":"autopilot-careers-de69a.firebaseapp.com","messagingSenderId":"1012708335965"};
-
-let firebaseApp: FirebaseApp;
-if (!getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig);
-} else {
-  firebaseApp = getApp();
-}
-
-export { firebaseApp };
+export const firebaseConfig = {
+  "projectId": "studio-8467801333-7bc91",
+  "appId": "1:1041837830391:web:7a8f984cfcc60bbef023f2",
+  "apiKey": "AIzaSyCuuTpfHEukgnGgeH3BJX0Y0EOoBITyf3Y",
+  "authDomain": "studio-8467801333-7bc91.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1041837830391"
+};
